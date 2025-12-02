94db5ddf94f87d6b92f6553aed9600a0d4729992
jake-w-liu <126843050+jake-w-liu@users.noreply.github.com>
2025-12-02 06:31:41 +0000

    Deploying to gh-pages from @ jake-w-liu/jake-w-liu.github.io@9af874463ac4c7860fea7f94193a9da948ac7e76 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -54,6 +54,11 @@ ninja.data = [{
           description: "graduate course",
           section: "Courses",handler: () => {
               window.location.href = "/courses/course_emt/";
+            },},{id: "courses-fourier-transform",
+          title: 'Fourier Transform',
+          description: "graduate course",
+          section: "Courses",handler: () => {
+              window.location.href = "/courses/course_fourier/";
             },},{id: "courses-geometrical-theory-of-diffraction",
           title: 'Geometrical Theory of Diffraction',
           description: "graduate course",
