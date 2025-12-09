75d5141fc9348a674309b7e7725dad5d0768fe0a
jake-w-liu <126843050+jake-w-liu@users.noreply.github.com>
2025-12-09 01:20:28 +0000

    Deploying to gh-pages from @ jake-w-liu/jake-w-liu.github.io@073bb634d1ccd59686c5c61af175f0b8956fcf64 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/courses/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-lab",
           title: "lab",
           description: "",
