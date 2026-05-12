6a5012f53756c539cf946f5e9601b315b31ea799
jake-w-liu <126843050+jake-w-liu@users.noreply.github.com>
2026-05-12 09:09:15 +0000

    Deploying to gh-pages from @ jake-w-liu/jake-w-liu.github.io@283a8ce9d44c7831edec0b039322b94a8ec7fe89 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -106,13 +106,6 @@ ninja.data = [{
           window.open("https://orcid.org/0000-0001-5458-7917", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
